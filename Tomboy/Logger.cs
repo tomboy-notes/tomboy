82acb33ffa10b3e0acba60c650552ad0468d5d11
Alex Graveley <orph@src.gnome.org>
2006-09-19 19:05:52 +0000

    2006-09-19  Alex Graveley  <alex@beatniksoftware.com>
    	* Tomboy/Logger.cs: Patch from Jeff Rose to output timestamped log
    	data to ~/.tomboy/log and to the console.

--- Tomboy/Logger.cs
+++ Tomboy/Logger.cs
@@ -1,54 +1,134 @@
 using System;
+using System.IO;
 
 namespace Tomboy
 {
+	public enum Level { DEBUG, INFO, WARN, ERROR, FATAL };
+
 	public interface ILogger
 	{
-		void Log (string msg, params object[] args);
+		void Log (Level lvl, string msg, params object[] args);
 	}
 
 	class NullLogger : ILogger
 	{
-		public void Log (string msg, params object[] args)
+		public void Log (Level lvl, string msg, params object[] args)
 		{
 		}
 	}
 
 	class ConsoleLogger : ILogger
 	{
-		public void Log (string msg, params object[] args)
+		public void Log (Level lvl, string msg, params object[] args)
 		{
+			msg = string.Format ("[{0}]: {1}", Enum.GetName (typeof (Level), lvl), msg);
 			Console.WriteLine (msg, args);
 		}
 	}
 
-	// This class provides a generic logging facility. By default all
-	// information is written to stdout, but other loggers are pluggable.
-	// TODO: The generic Log method should probably be replaced by methods
-	// like Debug, Message, and Error.
-	public static class Logger
+	class FileLogger : ILogger
 	{
-		static ILogger instance = new ConsoleLogger ();
+		StreamWriter log;
+		ConsoleLogger console;
 
-		public static ILogger Instance
+		public FileLogger()
 		{
-			get { return instance; }
-			set { instance = value; }
+			log = File.CreateText (Path.Combine (Path.Combine (
+				Environment.GetEnvironmentVariable ("HOME"), 
+				".tomboy"), "log"));
+			log.Flush();
+
+			console = new ConsoleLogger();
 		}
 
+		~FileLogger()
+		{
+			log.Flush();
+		}
+
+		public void Log (Level lvl, string msg, params object[] args)
+		{
+			console.Log (lvl, msg, args);
+
+			msg = string.Format ("{0} [{1}]: {2}", 
+					     DateTime.Now.ToString(), 
+					     Enum.GetName (typeof (Level), lvl), 
+					     msg);
+			log.WriteLine (msg, args);
+			log.Flush();
+		}
+	}
+
+	// This class provides a generic logging facility. By default all
+	// information is written to standard out and a log file, but other 
+	// loggers are pluggable.
+	public static class Logger
+	{
+		private static Level log_level = Level.DEBUG;
+
+		static ILogger log_dev = new FileLogger();
+
+		static bool muted = false;
+
+		public static Level LogLevel
+		{
+			get { return log_level; }
+			set { log_level = value; }
+		}
+
+		public static ILogger LogDevice
+		{
+			get { return log_dev; }
+			set { log_dev = value; }
+		}
+
+		public static void Debug (string msg, params object[] args)
+		{
+			Log (Level.DEBUG, msg, args);
+		}
+
+		public static void Info (string msg, params object[] args)
+		{
+			Log (Level.INFO, msg, args);
+		}
+
+		public static void Warn (string msg, params object[] args)
+		{
+			Log (Level.WARN, msg, args);
+		}
+
+		public static void Error (string msg, params object[] args)
+		{
+			Log (Level.ERROR, msg, args);
+		}
+
+		public static void Fatal (string msg, params object[] args)
+		{
+			Log (Level.FATAL, msg, args);
+		}
+
+		public static void Log (Level lvl, string msg, params object[] args)
+		{
+			if (!muted && lvl >= log_level)
+				log_dev.Log (lvl, msg, args);
+		}
+
+		// This is here to support the original logging, but it should be
+		// considered deprecated and old code that uses it should be upgraded to
+		// call one of the level specific log methods.
 		public static void Log (string msg, params object[] args)
 		{
-			instance.Log (msg, args);
+			Log (Level.DEBUG, msg, args);
 		}
 
 		public static void Mute ()
 		{
-			instance = new NullLogger ();
+			muted = true;
 		}
 
 		public static void Unmute ()
 		{
-			instance = new ConsoleLogger ();
+			muted = false;
 		}
 	}
 }
