220c268b8690640faf36b41a40ca4ae42f13b2c4
Boyd Timothy <btimothy@gmail.com>
2007-03-02 21:26:23 +0000

    Respect Emacs keybindings for Ctrl-W and Ctrl-N. Fixes bug #412402.
    2007-03-02  Boyd Timothy  <btimothy@gmail.com>
    
    	* Tomboy/NoteWindow.cs: Respect Emacs keybindings for Ctrl-W and Ctrl-N.
    	Fixes bug #412402.
    
    svn path=/trunk/; revision=1028

--- Tomboy/NoteWindow.cs
+++ Tomboy/NoteWindow.cs
@@ -277,7 +277,14 @@ namespace Tomboy
 			box.PackStart (editor_window, true, true, 0);
 			box.PackStart (find_bar, false, false, 0);
 			box.Show ();
-
+			
+			// Don't set up Ctrl-W or Ctrl-N if Emacs is in use
+			bool using_emacs = false;
+			string gtk_key_theme = (string)
+					Preferences.Get ("/desktop/gnome/interface/gtk_key_theme");
+			if (gtk_key_theme != null && gtk_key_theme.CompareTo ("Emacs") == 0)
+				using_emacs = true;
+			
 			// NOTE: Since some of our keybindings are only
 			// available in the context menu, and the context menu
 			// is created on demand, register them with the
@@ -285,7 +292,8 @@ namespace Tomboy
 			global_keys = new GlobalKeybinder (accel_group);
 
 			// Close window (Ctrl-W)
-			global_keys.AddAccelerator (new EventHandler (CloseWindowHandler),
+			if (!using_emacs)
+				global_keys.AddAccelerator (new EventHandler (CloseWindowHandler),
 						    (uint) Gdk.Key.w, 
 						    Gdk.ModifierType.ControlMask,
 						    Gtk.AccelFlags.Visible);
@@ -319,7 +327,8 @@ namespace Tomboy
 						    0);
 			
 			// Create a new note
-			global_keys.AddAccelerator (new EventHandler (CreateNewNote),
+			if (!using_emacs)
+				global_keys.AddAccelerator (new EventHandler (CreateNewNote),
 							(uint) Gdk.Key.n,
 							Gdk.ModifierType.ControlMask,
 							Gtk.AccelFlags.Visible);
