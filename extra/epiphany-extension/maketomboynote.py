c3f7b98621d66d94c7b1bcf8bec385fed8f8fcfc
Sanford Armstrong <sharm@src.gnome.org>
2007-04-16 15:01:13 +0000

    Added Tomboy icon to menu entry with a hard-coded path. If note with site
    * extra/epiphany-extension/maketomboynote.py: Added Tomboy icon to menu
      entry with a hard-coded path.  If note with site title already
      exists, open note but do not modify.
    
    svn path=/trunk/; revision=1116

--- extra/epiphany-extension/maketomboynote.py
+++ extra/epiphany-extension/maketomboynote.py
@@ -7,7 +7,7 @@ import dbus
 import gtk
 import epiphany
 
-ui_str = """
+_ui_str = """
 <ui>
   <menubar name="menubar">
     <menu name="ToolsMenu" action="Tools">
@@ -18,6 +18,10 @@ ui_str = """
   </menubar>
 </ui>
 """
+
+
+_tomboy_stock_icon = 'tomboy-stock-note'
+_icons_initialized = False
 
 # TODO: There is basically no error checking here
 def _menu_entry_cb(action, window):
@@ -30,24 +34,32 @@ def _menu_entry_cb(action, window):
 	active_title = embed.get_title()
 	note_title = active_title[0:50] # Trim after 50 characters
 
-	uri = tomboy.CreateNamedNote(note_title)
-	if uri is not  "":
+	uri = tomboy.FindNote(note_title) # Check for existing note
+	if uri != "":
 		tomboy.DisplayNote(uri)
-		# NOTE: Tomboy bug makes it so you can't set links properly,
-		#	but if note is open it will automatically linkify
-		tomboy.SetNoteContents(uri, note_title + "\n\n" + active_url)
+		#TODO: Modify contents at all?
 	else:
-		pass #error dialog?
+		uri = tomboy.CreateNamedNote(note_title)
+		if uri !=  "":
+			tomboy.DisplayNote(uri)
+			# NOTE: Tomboy bug makes it so you can't set links properly,
+			#	but if note is open it will automatically linkify
+			tomboy.SetNoteContents(uri, note_title + "\n\n" + active_url)
+		else:
+			pass #error dialog?
 
-_actions = [('MakeTomboyNote', None, 'Note This in Tomboy!',
+
+_actions = [('MakeTomboyNote', _tomboy_stock_icon, 'Note This in Tomboy!',
 	     None, None, _menu_entry_cb)]
 
 def attach_window(window):
+	if not _icons_initialized:
+		init_icons()
 	ui_manager = window.get_ui_manager()
 	group = gtk.ActionGroup('MakeTomboyNote')
 	group.add_actions(_actions, window)
 	ui_manager.insert_action_group(group, 0)
-	ui_id = ui_manager.add_ui_from_string(ui_str)
+	ui_id = ui_manager.add_ui_from_string(_ui_str)
 
 	window._make_tomboy_note_data = (group, ui_id)
 
@@ -59,3 +71,12 @@ def detach_window(window):
 	ui_manager.remove_ui(ui_id)
 	ui_manager.remove_action_group(group)
 	ui_manager.ensure_update()
+
+def init_icons():
+	factory = gtk.IconFactory()
+	fn = "/usr/share/icons/hicolor/16x16/apps/tomboy.png"	#TODO: Generalize
+	pixbuf = gtk.gdk.pixbuf_new_from_file(fn)
+	set = gtk.IconSet(pixbuf)
+	factory.add(_tomboy_stock_icon, set)
+	factory.add_default()
+	_icons_initialized = True
