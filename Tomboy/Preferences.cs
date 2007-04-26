d9d8c96ad46c09f9ef5792ae40d22658d9c026f7
Sanford Armstrong <sharm@src.gnome.org>
2007-04-26 16:58:31 +0000

    Make Tomboy remember open/closed state of notes in between sessions. The
    * Tomboy/NoteManager.cs, Tomboy/Preferences.cs, Tomboy/Note.cs,
      data/tomboy.schemas.in: Make Tomboy remember open/closed state of
      notes in between sessions.  The GConf pref to enable/disable this
      feature (/apps/tomboy/enable_startup_notes) is not exposed in the
      UI yet, but it defaults to enabled.  Fixes bug #305118 (patch from
      Stefan Schweizer).
    
    svn path=/trunk/; revision=1154

--- Tomboy/Preferences.cs
+++ Tomboy/Preferences.cs
@@ -17,6 +17,7 @@ namespace Tomboy
 		public const string ENABLE_WIKIWORDS = "/apps/tomboy/enable_wikiwords";
 		public const string ENABLE_CUSTOM_FONT = "/apps/tomboy/enable_custom_font";
 		public const string ENABLE_KEYBINDINGS = "/apps/tomboy/enable_keybindings";
+		public const string ENABLE_STARTUP_NOTES = "/apps/tomboy/enable_startup_notes";
 
 		public const string START_NOTE_URI = "/apps/tomboy/start_note";
 		public const string CUSTOM_FONT_FACE = "/apps/tomboy/custom_font_face";
@@ -102,6 +103,9 @@ namespace Tomboy
 				return "";
 
 			case STICKYNOTEIMPORTER_FIRST_RUN:
+				return true;
+			
+			case ENABLE_STARTUP_NOTES:
 				return true;
 			}
 
