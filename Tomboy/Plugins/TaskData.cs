cd83a23606bc69bcccffe311e2d3a1842d247722
Boyd H Timothy <btimothy@src.gnome.org>
2007-05-31 15:05:39 +0000

    * Tomboy.mdp, Tomboy/Plugins/TaskArchiver.cs,   Tomboy/Plugins/checkbox-done.png, Tomboy/Plugins/TaskData.cs,   Tomboy/Plugins/TaskManager.cs, Tomboy/Plugins/TaskTag.cs,   Tomboy/Plugins/Task.cs, Tomboy/Plugins/TasksPlugin.cs,   Tomboy/Plugins/Makefile.am, Tomboy/Plugins/checkbox-todo.png: Added   Tomboy/Plugins/TasksPlugin.cs, Tomboy/Plugins/Task.cs,   Tomboy/Plugins/TaskManager.cs, Tomboy/Plugins/TaskData.cs,   Tomboy/Plugins/TaskArchiver.cs, Tomboy/Plugins/TaskTag.cs,   Tomboy/Plugins/checkbox-done.png, Tomboy/Plugins/checkbox-todo.png.
      This is some initial work on the Tasks Plugin.  The TaskManager now
      loads, is able to create/delete tasks.  No UI is built on top of
      this yet.  That's the next step.
    
    svn path=/trunk/; revision=1181

---
+++ Tomboy/Plugins/TaskData.cs
@@ -0,0 +1,77 @@
+
+using System;
+using Tomboy;
+
+/// <summary>
+/// Contains all the task data, like the summary, details, etc.
+/// </summary>
+public class TaskData
+{
+	readonly string uri;
+	string summary;
+	string details;
+	DateTime create_date;
+	DateTime last_change_date;
+	DateTime due_date;
+	DateTime completion_date;
+	TaskPriority priority;
+
+	public TaskData (string uri)
+	{
+		this.uri = uri;
+		this.details = string.Empty;
+		
+		create_date = DateTime.MinValue;
+		last_change_date = DateTime.MinValue;
+		due_date = DateTime.MinValue;
+		completion_date = DateTime.MinValue;
+		priority = TaskPriority.Normal;
+	}
+
+	public string Uri
+	{
+		get { return uri; }
+	}
+
+	public string Summary
+	{
+		get { return summary; }
+		set { summary = value; }
+	}
+
+	public string Details
+	{
+		get { return details; }
+		set { details = value; }
+	}
+
+	public DateTime CreateDate
+	{
+		get { return create_date; }
+		set { create_date = value; }
+	}
+
+	public DateTime LastChangeDate
+	{
+		get { return last_change_date; }
+		set { last_change_date = value; }
+	}
+	
+	public DateTime DueDate
+	{
+		get { return due_date; }
+		set { due_date = value; }
+	}
+	
+	public DateTime CompletionDate
+	{
+		get { return completion_date; }
+		set { completion_date = value; }
+	}
+	
+	public TaskPriority Priority
+	{
+		get { return priority; }
+		set { priority = value; }
+	}
+}
---
+++ Tomboy/Plugins/TaskTag.cs
@@ -0,0 +1,65 @@
+
+using System;
+using Tomboy;
+
+public class TaskTag : DynamicNoteTag
+{
+	static Gdk.Pixbuf todo_icon;
+	static Gdk.Pixbuf done_icon;
+	
+	static TaskTag ()
+	{
+		// Load the pixbufs directly from the assembly's resources
+		done_icon = new Gdk.Pixbuf (null, "checkbox-done.png");
+		todo_icon = new Gdk.Pixbuf (null, "checkbox-todo.png");
+	}
+
+	public TaskTag () : base ()
+	{
+	}
+
+	public override void Initialize (string element_name)
+	{
+		base.Initialize (element_name);
+
+//		Underline = Pango.Underline.Single;
+		Foreground = "green"; // This is temporary just so during debugging we can visualize the tag
+		Editable = true;
+		CanActivate = true;
+		CanGrow = true;
+		CanSpellCheck = true;
+		CanSplit = false;
+	}
+
+	protected override bool OnActivate (NoteEditor editor, Gtk.TextIter start, Gtk.TextIter end)
+	{
+		string uri = Attributes ["uri"] as string;
+		if (uri == null)
+			return false;
+		
+		Logger.Debug ("FIXME: Implement Tasks.OnActivate: {0}", uri);
+		return true;
+	}
+	
+	public string Uri
+	{
+		get { return (string) Attributes ["uri"]; }
+		set { Attributes ["uri"] = value; }
+	}
+	
+	public bool Completed
+	{
+		get {
+			bool completed = false;
+			string completed_str = (string) Attributes ["completed"];
+			if (completed_str != null)
+				completed = Boolean.Parse (completed_str);
+
+			return completed;
+		}
+		set {
+			Attributes ["completed"] = value.ToString ();
+		}
+	}
+}
+
