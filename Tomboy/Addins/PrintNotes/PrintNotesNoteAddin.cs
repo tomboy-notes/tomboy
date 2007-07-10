b66bc927070aca42e4f58526cdd9e24a76b87f0c
Boyd H Timothy <btimothy@src.gnome.org>
2007-07-10 21:27:33 +0000

    * configure.in: Addd PrintNotes addin Makefile. * Tomboy.mdp: Remove the old PrintNotes plugin and replace it with the   PrintNotes addin. * Tomboy/Plugins/PrintNotes.cs: Moved this to   Tomboy/Addins/PrintNotes/PrintNotesNoteAddin.cs. * Tomboy/Addins/PrintNotes,   Tomboy/Addins/PrintNotes/PrintNotesNoteAddin.cs,   Tomboy/Addins/PrintNotes/PrintNotes.addin.xml,   Tomboy/Addins/PrintNotes/Makefile.am: The new PrintNotes addin. * Tomboy/Addins/Makefile.am: Add PrintNotes to SUBDIRS and also split   it out onto a separate line since it was beginning to be too long. * po/POTFILES.in: Replace the old PrintNotes plugin source with the new   addin.
    svn path=/trunk/; revision=1306

---
+++ Tomboy/Addins/PrintNotes/PrintNotesNoteAddin.cs
@@ -0,0 +1,49 @@
+
+using System;
+using System.Runtime.InteropServices;
+using Mono.Unix;
+
+using Gtk;
+
+using Tomboy;
+
+namespace Tomboy.PrintNotes
+{
+	public class PrintNotesNoteAddin : NoteAddin
+	{
+		Gtk.ImageMenuItem item;
+
+		public override void Initialize ()
+		{
+			item = new Gtk.ImageMenuItem (Catalog.GetString ("Print"));
+			item.Image = new Gtk.Image (Gtk.Stock.Print, Gtk.IconSize.Menu);
+			item.Activated += PrintButtonClicked;
+			item.Show ();
+			AddPluginMenuItem (item);
+		}
+
+		public override void Shutdown ()
+		{
+			// Disconnect the event handlers so
+			// there aren't any memory leaks.
+			item.Activated -= PrintButtonClicked;
+		}
+
+		public override void OnNoteOpened () 
+		{
+			// Do nothing.
+		}
+
+		[DllImport("libtomboy")]
+		static extern void gedit_print (IntPtr text_view_handle);
+
+		//
+		// Handle Print menu item Click
+		//
+
+		void PrintButtonClicked (object sender, EventArgs args)
+		{
+			gedit_print (Note.Window.Editor.Handle);
+		}
+	}
+}
