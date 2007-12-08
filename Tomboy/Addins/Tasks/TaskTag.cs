f059d57768d4ec397e6b35f83ca72fb8f7889a2e
Boyd Timothy <btimothy@gmail.com>
2007-12-08 05:11:01 +0000

    Adjust so the TaskTag will compile. Add new attributes so that we store
    2007-12-07  Boyd Timothy <btimothy@gmail.com> 
    
            * Tomboy/Addins/Tasks/TasksNoteAddin.cs: Adjust so the TaskTag
            * will
              compile.
            * Tomboy/Addins/Tasks/TaskTag.cs: Add new attributes so that we
            * store
              the properties in the note xml.
    
    
    svn path=/trunk/; revision=1662

--- Tomboy/Addins/Tasks/TaskTag.cs
+++ Tomboy/Addins/Tasks/TaskTag.cs
@@ -8,6 +8,13 @@ namespace Tomboy.Tasks
 	{
 		static Gdk.Pixbuf todo_icon;
 		static Gdk.Pixbuf done_icon;
+		
+		const string PROP_URI = "uri";
+		const string PROP_CREATION_DATE = "creation-date";
+		const string PROP_LAST_CHANGE_DATE = "last-change-date";
+		const string PROP_DUE_DATE = "due-date";
+		const string PROP_COMPLETION_DATE = "completion-date";
+		const string PROP_PRIORITY = "priority";
 
 		static TaskTag ()
 		{
@@ -16,7 +23,7 @@ namespace Tomboy.Tasks
 			todo_icon = new Gdk.Pixbuf (null, "checkbox-todo.png");
 		}
 
-public TaskTag () : base ()
+		public TaskTag () : base ()
 		{
 		}
 
@@ -36,7 +43,7 @@ public TaskTag () : base ()
 
 		public void UpdateStatus ()
 		{
-			if (Completed) {
+			if (CompletionDate != DateTime.MinValue) {
 				Foreground = "green"; // This is temporary so we can view state
 				Strikethrough = true;
 			} else {
@@ -48,29 +55,128 @@ public TaskTag () : base ()
 		public string Uri
 		{
 			get {
-				return (string) Attributes ["uri"];
+				return (string) Attributes [PROP_URI];
 			}
 			set {
-				Attributes ["uri"] = value;
+				Attributes [PROP_URI] = value;
 			}
 		}
-
-		public bool Completed
+		
+		public DateTime CreationDate
 		{
 			get {
-				bool completed = false;
-				string completed_str = (string) Attributes ["completed"];
-				if (completed_str != null)
-					completed = Boolean.Parse (completed_str);
-
-				return completed;
+				string date_str =  (string) Attributes [PROP_CREATION_DATE];
+				if (date_str == null)
+					return DateTime.MinValue;
+				else
+					return DateTime.Parse (date_str);
 			}
 			set {
-				Attributes ["completed"] = value.ToString ();
-				UpdateStatus ();
+				Attributes [PROP_CREATION_DATE] = ((DateTime)value).ToString ();
+			}
+		}
+		
+		public DateTime LastChangeDate
+		{
+			get {
+				string date_str =  (string) Attributes [PROP_LAST_CHANGE_DATE];
+				if (date_str == null)
+					return DateTime.MinValue;
+				else
+					return DateTime.Parse (date_str);
+			}
+			set {
+				Attributes [PROP_LAST_CHANGE_DATE] = ((DateTime)value).ToString ();
+			}
+		}
+		
+		public DateTime DueDate
+		{
+			get {
+				string date_str =  (string) Attributes [PROP_DUE_DATE];
+				if (date_str == null)
+					return DateTime.MinValue;
+				else
+					return DateTime.Parse (date_str);
+			}
+			set {
+				Attributes [PROP_DUE_DATE] = ((DateTime)value).ToString ();
 			}
 		}
 
+		public DateTime CompletionDate
+		{
+			get {
+				string date_str = (string) Attributes [PROP_COMPLETION_DATE];
+				if (date_str == null)
+					return DateTime.MinValue;
+				else
+					return DateTime.Parse (date_str);
+			}
+			set {
+				Attributes [PROP_COMPLETION_DATE] = ((DateTime)value).ToString ();
+			}
+		}
+
+		public TaskPriority TaskPriority
+		{
+			get {
+				TaskPriority priority;
+				string prop = (string) Attributes [PROP_COMPLETION_DATE];
+				switch (prop) {
+					case "low":
+						priority = TaskPriority.Low;
+						break;
+					case "normal":
+						priority = TaskPriority.Normal;
+						break;
+					case "high":
+						priority = TaskPriority.High;
+						break;
+					default:
+						priority = TaskPriority.Undefined;
+						break;
+				}
+				
+				return priority;
+			}
+			set {
+				string prop;
+				switch (value) {
+					case TaskPriority.Low:
+						prop = "low";
+						break;
+					case TaskPriority.Normal:
+						prop = "normal";
+						break;
+					case TaskPriority.High:
+						prop = "high";
+						break;
+					default:
+						prop = "undefined";
+						break;
+				}
+				
+				Attributes [PROP_PRIORITY] = prop;
+			}
+		}
+
+//		public bool Complete
+//		{
+//			get {
+//				bool completed = false;
+//				string completed_str = (string) Attributes ["completed"];
+//				if (completed_str != null)
+//					completed = Boolean.Parse (completed_str);
+//
+//				return completed;
+//			}
+//			set {
+//				Attributes ["completed"] = value.ToString ();
+//				UpdateStatus ();
+//			}
+//		}
+		
 		/*
 		  public override Gdk.Pixbuf Image
 		  {
