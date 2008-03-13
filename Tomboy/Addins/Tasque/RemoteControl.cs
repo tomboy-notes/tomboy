7c93e8b626a9ac269e704d35ad0b1e8785841ca2
Boyd H Timothy <btimothy@src.gnome.org>
2008-03-13 04:34:16 +0000

    Added an addin for Tasque (disabled by default).
    * configure.in, Tomboy.mdp, Tomboy/Addins/Tasque,
      Tomboy/Addins/Tasque/Tasque.addin.xml,
      Tomboy/Addins/Tasque/RemoteControl.cs,
      Tomboy/Addins/Tasque/tasque-22.png,
      Tomboy/Addins/Tasque/Makefile.am,
      Tomboy/Addins/Tasque/TasqueNoteAddin.cs, Tomboy/Addins/Makefile.am:
      Added an addin for Tasque (disabled by default).
    
    svn path=/trunk/; revision=1965

---
+++ Tomboy/Addins/Tasque/RemoteControl.cs
@@ -0,0 +1,34 @@
+// RemoteControl.cs created with MonoDevelop
+// User: boyd at 9:41 PM 2/14/2008
+
+using System;
+using System.Collections.Generic;
+
+using NDesk.DBus;
+using org.freedesktop.DBus;
+
+namespace Tasque
+{
+	[Interface ("org.gnome.Tasque.RemoteControl")]
+	public class RemoteControl : MarshalByRefObject
+	{
+		public RemoteControl()
+		{
+		}
+		
+		public string CreateTask (string categoryName, string taskName,
+								  bool enterEditMode)
+		{
+			return null;
+		}
+		
+		public string[] GetCategoryNames ()
+		{
+			return null;
+		}
+		
+		public void ShowTasks ()
+		{
+		}
+	}
+}
