0b839ad0150764d6bcccd61026f3c6b6499d250f
Sanford Armstrong <sharm@src.gnome.org>
2008-05-12 07:12:52 +0000

    Update printing addin to use Gtk.Print. Delete old gedit/gnomeprint-based
    * Tomboy.mdp, Tomboy/Addins/PrintNotes/PrintNotesNoteAddin.cs,
      Tomboy/Addins/PrintNotes/PrintNotes.addin.xml,
      Tomboy/Addins/PrintNotes/Makefile.am: Update printing addin to use
      Gtk.Print.
    * Tomboy/Addins/PrintNotes/gtksourceprintjob.c,
      Tomboy/Addins/PrintNotes/gtksourceprintjob.h,
      Tomboy/Addins/PrintNotes/gedit-print.c,
      Tomboy/Addins/PrintNotes/COPYING,
      Tomboy/Addins/PrintNotes/gedit-print.h, COPYING: Delete old
      gedit/gnomeprint-based print code.
    * configure.in: Require gtk-sharp >= 2.10.
    
    svn path=/trunk/; revision=1997

--- Tomboy/Addins/PrintNotes/PrintNotesNoteAddin.cs
+++ Tomboy/Addins/PrintNotes/PrintNotesNoteAddin.cs
@@ -1,22 +1,27 @@
 
 using System;
+using System.Collections.Generic;
 using System.Runtime.InteropServices;
 using Mono.Unix;
 
 using Gtk;
 
 using Tomboy;
-
+ 
 namespace Tomboy.PrintNotes
 {
 	public class PrintNotesNoteAddin : NoteAddin
 	{
-		Gtk.ImageMenuItem item;
-
+		private Gtk.ImageMenuItem item;
+		private int font_size = 12;			
+		private List<int> pageBreaks;
+		private Pango.Layout layout;
+	
 		public override void Initialize ()
 		{
 			item = new Gtk.ImageMenuItem (Catalog.GetString ("Print"));
-			item.Image = new Gtk.Image (Gtk.Stock.Print, Gtk.IconSize.Menu);
+			item.Image = new Gtk.Image (Gtk.Stock.Print,
+			                            Gtk.IconSize.Menu);
 			item.Activated += PrintButtonClicked;
 			item.Show ();
 			AddPluginMenuItem (item);
@@ -33,17 +38,126 @@ namespace Tomboy.PrintNotes
 		{
 			// Do nothing.
 		}
-
-		[DllImport("libprintnotes")]
-		static extern void gedit_print (IntPtr text_view_handle);
-
-		//
-		// Handle Print menu item Click
-		//
-
-		void PrintButtonClicked (object sender, EventArgs args)
+		
+		private void PrintButtonClicked (object sender, EventArgs args)
 		{
-			gedit_print (Note.Window.Editor.Handle);
+			Gtk.PrintOperation op = new PrintOperation ();
+			op.BeginPrint += OnBeginPrint;
+			op.DrawPage += OnDrawPage;
+			op.EndPrint += OnEndPrint;
+			
+			op.Run (Gtk.PrintOperationAction.PrintDialog, this.Window);
+		}
+		
+		private void OnBeginPrint (object sender, Gtk.BeginPrintArgs args)
+		{
+			PrintContext context = args.Context;
+			double width = context.Width;
+			double height = context.Height;
+			
+			layout = context.CreatePangoLayout ();
+			
+			Pango.FontDescription desc =
+				Pango.FontDescription.FromString ("sans " +
+				                                  font_size);
+			layout.FontDescription = desc;
+			
+			layout.Width = Pango.Units.FromDouble (context.Width);
+			
+			Gtk.TextIter start_iter, end_iter;
+			Buffer.GetBounds (out start_iter, out end_iter);
+			layout.SetText (
+				Buffer.GetText (start_iter, end_iter, false));
+			
+			int numLines = layout.LineCount;
+			
+			pageBreaks = new List<int> ();
+			double pageHeight = 0;
+			
+			for (int i = 0; i < numLines; i++) {
+				Pango.Rectangle inkRect =
+					new Pango.Rectangle ();
+				Pango.Rectangle logicalRect =
+					new Pango.Rectangle ();
+				
+				Pango.LayoutLine layoutLine =
+					layout.GetLine (i);
+				layoutLine.GetExtents (ref inkRect,
+				                       ref logicalRect);
+				
+				double lineHeight = logicalRect.Height / 1024.0;
+				
+				if (pageHeight + lineHeight > height) {
+					pageBreaks.Add (i);
+					pageHeight = 0;
+				}
+				
+				pageHeight += lineHeight;
+			}
+			
+			PrintOperation op = (PrintOperation) sender;
+			op.NPages = pageBreaks.Count + 1;
+		}
+		
+		public void OnDrawPage (object sender, Gtk.DrawPageArgs args)
+		{
+			Cairo.Context cr = args.Context.CairoContext;
+			
+			int start, end, i;
+			
+			if (args.PageNr == 0)
+				start = 0;
+			else
+				start = pageBreaks [args.PageNr - 1];
+			
+			if (pageBreaks.Count <= args.PageNr)
+				end = layout.LineCount;
+			else
+				end = pageBreaks [args.PageNr];
+			
+			i = 0;
+			Pango.LayoutIter iter = layout.Iter;
+			do {
+				int baseline = 0;
+				
+				if (i >= start) {
+					Pango.LayoutLine line = iter.Line;
+					Pango.Rectangle logicalRect =
+						new Pango.Rectangle ();
+					Pango.Rectangle dummyRect =
+						new Pango.Rectangle ();
+
+					line.GetExtents (ref dummyRect, 
+					                 ref logicalRect);
+					baseline = iter.Baseline;
+					
+					if (i == start) {
+						cr.MoveTo (0, 0);
+					}
+					
+					Pango.CairoHelper.ShowLayoutLine (cr, line);
+					
+					int height, width;
+					layout.GetSize (out width, out height);
+					cr.RelMoveTo (0, (int)
+					              (logicalRect.Height /
+					               Pango.Scale.PangoScale));
+				}
+				
+				i++;
+			}			
+			while (i < end && iter.NextLine ());
+		}
+		
+		private void OnEndPrint (object sender, Gtk.EndPrintArgs args)
+		{
+			layout = null;
+			pageBreaks.Clear ();
+			
+			// TODO: How to fix this error when quitting?
+			// "Cairo.Context: called from finalization thread
+			// programmer is missing a call to Dispose"
+			// May be a cairo/pango/gtk-sharp problem?
 		}
 	}
 }
