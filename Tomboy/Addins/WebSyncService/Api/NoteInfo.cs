b25696418a2133224a779c520548ffebb811f59e
Sandy Armstrong <sanfordarmstrong@gmail.com>
2009-06-29 08:22:08 -0700

    Fix year in file copyrights.

--- Tomboy/Addins/WebSyncService/Api/NoteInfo.cs
+++ Tomboy/Addins/WebSyncService/Api/NoteInfo.cs
@@ -17,7 +17,7 @@
 // OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION 
 // WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE. 
 // 
-// Copyright (c) 2008 Novell, Inc. (http://www.novell.com) 
+// Copyright (c) 2009 Novell, Inc. (http://www.novell.com) 
 // 
 // Authors: 
 //      Sandy Armstrong <sanfordarmstrong@gmail.com>
