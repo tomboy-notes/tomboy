a4bb49a4e95c03e3253c4cf0c7ec9dc6e2ff546e
Sandy Armstrong <sanfordarmstrong@gmail.com>
2009-07-27 16:08:00 -0700

    Bump version to 0.15.5.

--- Tomboy/Defines.WIN32.cs
+++ Tomboy/Defines.WIN32.cs
@@ -2,11 +2,11 @@
 using System.Reflection;
 
 // TODO: Automate this
-[assembly: AssemblyInformationalVersion ("0.15.4")]
+[assembly: AssemblyInformationalVersion ("0.15.5")]
 
 namespace Tomboy {
 	public class Defines {
-		public const string VERSION = "0.15.4";
+		public const string VERSION = "0.15.5";
 		public static readonly string DATADIR = System.IO.Path.GetDirectoryName (Assembly.GetExecutingAssembly ().Location);
 		public static readonly string GNOME_LOCALE_DIR = System.IO.Path.Combine (DATADIR, "locale");
 		public const string GNOME_HELP_DIR = "@datadir@/gnome/help/tomboy";
