6312b8767eb9705a55afde44a68be7bc13f0df2a
Sandy Armstrong <sanfordarmstrong@gmail.com>
2010-02-22 13:57:41 -0500

    Bump version to 1.1.4

--- Tomboy/Defines.WIN32.cs
+++ Tomboy/Defines.WIN32.cs
@@ -2,13 +2,13 @@
 using System.Reflection;
 
 // TODO: Automate this
-[assembly: AssemblyInformationalVersion ("1.1.3")]
+[assembly: AssemblyInformationalVersion ("1.1.4")]
 [assembly: AssemblyProduct("Tomboy")]
 [assembly: AssemblyTitle("Tomboy Notes")]
 
 namespace Tomboy {
 	public class Defines {
-		public const string VERSION = "1.1.3";
+		public const string VERSION = "1.1.4";
 		public static readonly string DATADIR = System.IO.Path.GetDirectoryName (Assembly.GetExecutingAssembly ().Location);
 		public static readonly string GNOME_LOCALE_DIR = System.IO.Path.Combine (DATADIR, "locale");
 		public const string GNOME_HELP_DIR = "@datadir@/gnome/help/tomboy";
