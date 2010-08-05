8f07f49ab71f095601229e3ab81af81fad5d9977
Benjamin Podszun <benjamin.podszun@topimagesystems.com>
2010-08-05 15:55:39 +0200

    [Setup] Improve Tomboy's presentation for Add/Remove programs
    Add the Tomboy icon as program icon to the installer database.
    Add the link to the Tomboy end-user site as help link.
    
    Define the FileVersion for the resulting Tomboy.exe assembly and
    automatically use that version as ProductVersion in the MSI table.
    Drop the AssemblyInformationalVersion and base Defines.VERSION on
    the assembly version again, reducing the number of lines to change
    for a new windows version to exactly one.

--- Tomboy/Defines.WIN32.cs
+++ Tomboy/Defines.WIN32.cs
@@ -2,13 +2,13 @@
 using System.Reflection;
 
 // TODO: Automate this
-[assembly: AssemblyInformationalVersion ("1.3.2")]
+[assembly: AssemblyVersion ("1.3.2")]
 [assembly: AssemblyProduct("Tomboy")]
 [assembly: AssemblyTitle("Tomboy Notes")]
 
 namespace Tomboy {
 	public class Defines {
-		public const string VERSION = "1.3.2";
+		public static readonly string VERSION = Assembly.GetExecutingAssembly().GetName().Version.ToString(3);
 		public static readonly string DATADIR = System.IO.Path.GetDirectoryName (Assembly.GetExecutingAssembly ().Location);
 		public static readonly string GNOME_LOCALE_DIR = System.IO.Path.Combine (DATADIR, "locale");
 		public const string GNOME_HELP_DIR = "@datadir@/gnome/help/tomboy";
