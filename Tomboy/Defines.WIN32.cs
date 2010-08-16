3014c9bc5f8172f0fde2962f3238288e30936f7e
Benjamin Podszun <benjamin.podszun@gmail.com>
2010-08-16 23:35:36 +0200

    Bump version to 1.3.3

--- Tomboy/Defines.WIN32.cs
+++ Tomboy/Defines.WIN32.cs
@@ -2,7 +2,7 @@
 using System.Reflection;
 
 // TODO: Automate this
-[assembly: AssemblyVersion ("1.3.2")]
+[assembly: AssemblyVersion ("1.3.3")]
 [assembly: AssemblyProduct("Tomboy")]
 [assembly: AssemblyTitle("Tomboy Notes")]
 
