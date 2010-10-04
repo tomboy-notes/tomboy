be37afad2b24735d7bb9894a285a0ba406b82eac
Benjamin Podszun <benjamin.podszun@topimagesystems.com>
2010-10-04 21:07:10 +0200

    Bump version to 1.4.2

--- Tomboy/Defines.WIN32.cs
+++ Tomboy/Defines.WIN32.cs
@@ -2,7 +2,7 @@
 using System.Reflection;
 
 // TODO: Automate this
-[assembly: AssemblyVersion ("1.4.1")]
+[assembly: AssemblyVersion ("1.4.2")]
 [assembly: AssemblyProduct("Tomboy")]
 [assembly: AssemblyTitle("Tomboy Notes")]
 
