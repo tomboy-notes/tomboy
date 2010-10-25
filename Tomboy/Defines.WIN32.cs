089add033b666c5ea91e69f2c4cfd176c08b8587
Sandy Armstrong <sanfordarmstrong@gmail.com>
2010-10-25 07:02:02 -0700

    Bump version to 1.5.3

--- Tomboy/Defines.WIN32.cs
+++ Tomboy/Defines.WIN32.cs
@@ -2,7 +2,7 @@
 using System.Reflection;
 
 // TODO: Automate this
-[assembly: AssemblyVersion ("1.5.2")]
+[assembly: AssemblyVersion ("1.5.3")]
 [assembly: AssemblyProduct("Tomboy")]
 [assembly: AssemblyTitle("Tomboy Notes")]
 
