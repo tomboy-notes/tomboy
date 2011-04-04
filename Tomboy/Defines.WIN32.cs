29c937c986a8ac4f7074434d2af6b7a7b323d974
Greg Poirier <gpoirier@src.gnome.org>
2011-04-04 17:18:44 -0500

    Bumped to version 1.6.1

--- Tomboy/Defines.WIN32.cs
+++ Tomboy/Defines.WIN32.cs
@@ -2,7 +2,7 @@
 using System.Reflection;
 
 // TODO: Automate this
-[assembly: AssemblyVersion ("1.6.0")]
+[assembly: AssemblyVersion ("1.6.1")]
 [assembly: AssemblyProduct("Tomboy")]
 [assembly: AssemblyTitle("Tomboy Notes")]
 
