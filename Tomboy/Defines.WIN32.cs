36df7141e5697778b028719a07d2dd9f78c4fb6f
Aaron Borden <adborden@live.com>
2011-07-25 14:53:48 -0700

    Bump the version to 1.7.3

--- Tomboy/Defines.WIN32.cs
+++ Tomboy/Defines.WIN32.cs
@@ -2,7 +2,7 @@
 using System.Reflection;
 
 // TODO: Automate this
-[assembly: AssemblyVersion ("1.7.2")]
+[assembly: AssemblyVersion ("1.7.3")]
 [assembly: AssemblyProduct("Tomboy")]
 [assembly: AssemblyTitle("Tomboy Notes")]
 
