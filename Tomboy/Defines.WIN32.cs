d4cfe7751298e65159b9747d2072dfa0f6e9816b
Jared Jennings <jjennings@src.gnome.org>
2011-10-17 17:15:42 -0400

    Bumping version to 1.9.2

--- Tomboy/Defines.WIN32.cs
+++ Tomboy/Defines.WIN32.cs
@@ -2,7 +2,7 @@
 using System.Reflection;
 
 // TODO: Automate this
-[assembly: AssemblyVersion ("1.9.1")]
+[assembly: AssemblyVersion ("1.9.2")]
 [assembly: AssemblyProduct("Tomboy")]
 [assembly: AssemblyTitle("Tomboy Notes")]
 
