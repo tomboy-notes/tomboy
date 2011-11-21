13ad3226fbaa3f70b492f58243c0f0b42f637dae
Aaron Borden <adborden@live.com>
2011-11-21 08:45:01 -0800

    Bump version to 1.9.4

--- Tomboy/Defines.WIN32.cs
+++ Tomboy/Defines.WIN32.cs
@@ -2,7 +2,7 @@
 using System.Reflection;
 
 // TODO: Automate this
-[assembly: AssemblyVersion ("1.9.3")]
+[assembly: AssemblyVersion ("1.9.4")]
 [assembly: AssemblyProduct("Tomboy")]
 [assembly: AssemblyTitle("Tomboy Notes")]
 
