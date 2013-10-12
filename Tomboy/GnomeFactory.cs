61aa66434a47d58c763e1e176cb5d2819b0d2af6
Robert Nordan <rpvn@robpvn.net>
2013-10-12 20:30:19 +0200

    Temp fix to make EVERYTHING work by using xml prefs instead of gConf
    Rollin' like it's Windows or Mac, oh yeah.

--- Tomboy/GnomeFactory.cs
+++ Tomboy/GnomeFactory.cs
@@ -2,23 +2,24 @@ namespace Tomboy
 {
 	public class GnomeFactory : IPlatformFactory
 	{
+		//FIXME: This needs to be properly ported to gSettings, this is just super temporary
 		public IPropertyEditor CreatePropertyEditorEntry (string key, Gtk.Entry sourceEntry)
 		{
 //			return new GConfPropertyEditorEntry (key, sourceEntry);
-			return null;
+			return new PropertyEditorEntry (key, sourceEntry);
 		}
 
 		public IPropertyEditorBool CreatePropertyEditorToggleButton (
 		        string key, Gtk.CheckButton sourceButton)
 		{
 //			return new GConfPropertyEditorToggleButton (key, sourceButton);
-			return null;
+			return new PropertyEditorBool (key);
 		}
 
 		public IPreferencesClient CreatePreferencesClient ()
 		{
 //			return new GConfPreferencesClient ();
-			return new NullPreferencesClient ();
+			return new XmlPreferencesClient ();
 		}
 
 		public INativeApplication CreateNativeApplication ()
