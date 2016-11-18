9bba6ac387dc1bdb08bc3a5957216259bdb1f509
Alex Tereschenko <frozen.and.blue@gmail.com>
2016-11-18 21:58:56 +0100

    configure: fix building on Debian and derivatives with DBus 2.0
    Inspired by Debian's distro-level Tomboy source package patch
    by Iain Lane <laney@debian.org>.
    
    Fixes #9.
    
    Signed-off-by: Alex Tereschenko <frozen.and.blue@gmail.com>

--- Tomboy/RemoteControlProxy.cs
+++ Tomboy/RemoteControlProxy.cs
@@ -55,8 +55,7 @@ namespace Tomboy
 				return null;
 
 			RemoteControl remote_control = new RemoteControl (manager);
-			Bus.Session.Register (Namespace,
-			                      new ObjectPath (Path),
+			Bus.Session.Register (new ObjectPath (Path),
 			                      remote_control);
 			return remote_control;
 #else
